37395ca27abd6bf5e9ad9e2d95bc43e2523519ef
kqlqk <kononchuk99999@gmail.com>
2023-10-05 19:03:08 +0200

    init and Roman to Integer task

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+package main.java;
+
+public class Main {
+    // Roman to Integer
+
+    public static void main(String[] args) {
+        System.out.println(new Main().romanToInt("MCMXCIV"));
+        System.out.println(new Main().romanToInt("LVIII"));
+    }
+
+    public int romanToInt(String s) {
+        char[] sChars = s.toCharArray();
+        int res = 0;
+
+        for (int i = sChars.length - 1; i >= 0 ; i--) {
+            int num = getNumberByRoman(sChars[i]);
+
+            if (i != 0 && getNumberByRoman(sChars[i - 1]) < num) {
+                res += num - getNumberByRoman(sChars[i-1]);
+                i--;
+            }
+            else {
+                res += num;
+            }
+        }
+
+        return res;
+    }
+
+    private int getNumberByRoman(char a) {
+        return switch (a) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> 0;
+        };
+    }
+}
+
