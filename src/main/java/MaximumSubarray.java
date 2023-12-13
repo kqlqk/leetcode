dfb8546005715e12ffc57efa329510e99e7a6a4e
kqlqk <kononchuk99999@gmail.com>
2023-12-13 16:30:27 +0100

    Maximum subarray task

---
+++ src/main/java/MaximumSubarray.java
@@ -0,0 +1,26 @@
+package main.java;
+
+public class MaximumSubarray {
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+
+
+    /*
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+     */
+
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
